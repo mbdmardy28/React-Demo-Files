2d9028011aa5e191f0675d96e48b0a38612b2842
Mardy Dacasin <MardyD@magenic.com>
2020-05-31 12:15:17 +0800

    Rename root reducer to counter reducer

--- Redux/redux-basic-typescript-final/src/redux/Store.ts
+++ Redux/redux-basic-typescript-final/src/redux/Store.ts
@@ -1,5 +1,5 @@
 import { createStore, applyMiddleware, Middleware } from "redux";
-import RootReducer from "./RootReducer";
+import CounterReducer from "./CounterReducer";
 import { composeWithDevTools } from "redux-devtools-extension";
 const middleware : Middleware[] = [];
-export const store = createStore(RootReducer, composeWithDevTools(applyMiddleware(...middleware)))
+export const store = createStore(CounterReducer, composeWithDevTools(applyMiddleware(...middleware)))
