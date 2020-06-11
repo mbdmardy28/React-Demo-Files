86ffcf695fd767a2f088853b86a5f35861c58b24
Mardy Dacasin <MardyD@magenic.com>
2020-06-11 13:54:00 +0800

    Add React Jest Unit Test Demo Start project

---
+++ Redux/react-jest-unit-test-typescript-start/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+export const sum = (value1: number, value2: number) => value1 + value2; 
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Hello React Unit Testing
+      </header>
+    </div>
+  );
+}
+
+export default App;
